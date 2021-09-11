ca4893f1c639f564915e8c41e000fd5d2a42acd4
Aryan Tayal <tayalaryan15@gmail.com>
2021-09-11 14:14:04 +0530

    Added lab1 solution

---
+++ binarysearch.java
@@ -0,0 +1,8 @@
+package com.lab01;
+
+public class binarysearch {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ bubblesort.java
@@ -0,0 +1,8 @@
+package com.lab01;
+
+public class bubblesort {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
